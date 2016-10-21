f4521948d337763f4225654c93f224e345b1a822
Simon de Haan <simon@praekeltfoundation.org>
2016-10-21 14:06:38 +0200

    wip

---
+++ README.rst
@@ -0,0 +1,16 @@
+Go Contacts Exporter
+=============================
+
+.. image:: https://img.shields.io/travis/smn/go-contacts-exporter.svg
+        :target: https://travis-ci.org/smn/go-contacts-exporter
+
+.. image:: https://img.shields.io/pypi/v/Go Contacts Exporter.svg
+        :target: https://pypi.python.org/pypi/Go Contacts Exporter
+
+.. image:: https://coveralls.io/repos/smn/go-contacts-exporter/badge.png?branch=develop
+    :target: https://coveralls.io/r/smn/go-contacts-exporter?branch=develop
+    :alt: Code Coverage
+
+.. image:: https://readthedocs.org/projects/Go Contacts Exporter/badge/?version=latest
+    :target: https://Go Contacts Exporter.readthedocs.org
+    :alt: Go Contacts Exporter Docs
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as req_file:
+    requirements = req_file.read().split('\n')
+
+with open('requirements-dev.txt') as req_file:
+    requirements_dev = req_file.read().split('\n')
+
+with open('VERSION') as fp:
+    version = fp.read().strip()
+
+setup(
+    name='go-contacts-exporter',
+    version=version,
+    description="This is the Go Contacts Exporter project.",
+    long_description=readme,
+    author="Praekelt Foundation",
+    author_email='dev@praekeltfoundation.org',
+    url='https://github.com/smn/go-contacts-exporter',
+    packages=[
+        'go-contacts-exporter',
+    ],
+    package_dir={'go-contacts-exporter':
+                 'go-contacts-exporter'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='go-contacts-exporter',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
