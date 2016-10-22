67416b19b1032946b4a7a5fa4d632688218b924f
Simon de Haan <simon@praekeltfoundation.org>
2016-10-22 11:09:59 +0200

    some msisdns contain junk

--- go_contacts_exporter/tables.py
+++ go_contacts_exporter/tables.py
@@ -6,6 +6,6 @@ contacts = Table(
     'contacts', metadata,
     Column('key', String(32), primary_key=True),
     Column('cursor', String(64)),
-    Column('msisdn', String(16)),
+    Column('msisdn', String(255)),
     Column('json', JSON),
 )
